92de2beacfdd18dea1e162c92bc6c0aaf1f9d2e8
Normando Ali Zubia Hernández <nzubia@blacklabs.mx>
2022-09-22 12:16:02 -0600

    Bootstrap template implemented
    * Bootstrap template was implemented in this Vue project
    * Creation of initial route for admin dashboard
    * Definition of admin routes for admin section

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,9 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 
+// Admin Views
+import AdminDashboard from "@/views/Admin/AdminDashboard";
+
 Vue.use(VueRouter)
 
 const routes = [
@@ -11,13 +14,18 @@ const routes = [
     component: HomeView
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+    path: '/admin',
+    name: 'admin',
+    component: AdminDashboard
+  },
+  // {
+  //   path: '/about',
+  //   name: 'about',
+  //   // route level code-splitting
+  //   // this generates a separate chunk (about.[hash].js) for this route
+  //   // which is lazy-loaded when the route is visited.
+  //   component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  // }
 ]
 
 const router = new VueRouter({
