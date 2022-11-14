ba9d0f46bc6f1344f16b3513f114567a601846f4
AlejandroCasrico <you@example.com>
2022-11-13 21:18:44 -0700

    nuevas ventanas de login edicion y agregar casas

--- src/views/Admin/EditHousesView.vue
+++ src/views/Admin/EditHousesView.vue
@@ -4,33 +4,55 @@
         title="Añadir Casa"
         subtitle="Formulario de creación de casa"
     ></BasicTitle>
-
     <div class="row mt-5">
       <div class="col">
         <form class="user">
           <div class="form-group row">
-            <div class="col-sm-6 mb-3 mb-sm-0">
+            <div  class="col-sm-6 mb-3 mb-sm-0">
+              
               <input type="text" class="form-control form-control-user" id="houseTitle"
-                     placeholder="Title">
+                     placeholder="Name">
+            </div>
+            <div  class="col-sm-6">
+              <input type="text" class="form-control form-control-user" id="exampleLastName"
+                     placeholder="type">
+            </div>
+            <div class="col-sm-6 mb-3 mb-sm-0">
+              <input type="text" class="form-control form-control-user" id="restrooms"
+                     placeholder="restroms">
             </div>
             <div class="col-sm-6">
-              <input type="text" class="form-control form-control-user" id="exampleLastName"
-                     placeholder="Last Name">
+              <input type="text" class="form-control form-control-user" id="bedroom"
+                     placeholder="bedroom">
+            </div>
+            <div class="col-sm-6">
+              <input type="text" class="form-control form-control-user" id="direction"
+                     placeholder="direction">
+            </div>
+            <div class="col-sm-6 mb-3 mb-sm-0">
+              <input type="text" class="form-control form-control-user" id="state"
+                     placeholder="state">
+            </div>
+            <div class="col-sm-6">
+              <input type="text" class="form-control form-control-user" id="meters"
+                     placeholder="meters">
+            </div>
+            <div class="col-sm-6 mb-3 mb-sm-0">
+              <input type="text" class="form-control form-control-user" id="price"
+                     placeholder="pets">
+            </div>
+            <div class="col-sm-6">
+              <input type="text" class="form-control form-control-user" id="location"
+                     placeholder="location">
+            </div>
+            <div class="col-sm-6">
+              <input type="text" class="form-control form-control-user" id="children"
+                     placeholder="children">
             </div>
           </div>
           <div class="form-group">
             <input type="email" class="form-control form-control-user" id="exampleInputEmail"
-                   placeholder="Email Address">
-          </div>
-          <div class="form-group row">
-            <div class="col-sm-6 mb-3 mb-sm-0">
-              <input type="password" class="form-control form-control-user"
-                     id="exampleInputPassword" placeholder="Password">
-            </div>
-            <div class="col-sm-6">
-              <input type="password" class="form-control form-control-user"
-                     id="exampleRepeatPassword" placeholder="Repeat Password">
-            </div>
+                   placeholder="description">
           </div>
           <a href="#" class="btn btn-primary btn-user btn-block">
             Save House
@@ -44,12 +66,24 @@
 
 <script>
 import BasicTitle from "@/components/BasicTitle";
-
+import axios from "axios";
 export default {
   name: "EditHousesView",
   components: {
     BasicTitle
-  }
+  },
+  data(){
+    return{
+   result:null
+    }
+  },
+  methods:{
+    async getAllHouses(){
+      const result = await axios.get("http://localhost:3000/houses");
+
+      this.result = result.data;
+    }
+}
 }
 </script>
 
